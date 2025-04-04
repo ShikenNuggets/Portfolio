056ef2bbbbcf925193ba45e3b6751d17070facf4
ShikenNuggets <carter.rennick@gmail.com>
2025-04-03 21:15:24 -0400

    ProjectContent as a separate component

--- src/pages/projects/whbg.tsx
+++ src/pages/projects/whbg.tsx
@@ -5,29 +5,25 @@ import { Helmet } from 'react-helmet';
 import Navbar from "@/components/Navbar";
 import MarkdownText from "@/components/MarkdownText";
 import getStaticPropsFromFile from '@/utils/FileContentsHelper'
+import ProjectContent from "@/components/ProjectContent";
 
 const inter = Inter({
-  variable: "--font-inter-sans",
-  weight: ["400", "500", "600", "700"],
-  subsets: ["latin"],
+	variable: "--font-inter-sans",
+	weight: ["400", "500", "600", "700"],
+	subsets: ["latin"],
 });
 
 const TITLE = 'Weird Hamster Ball Game - Games by Carter';
 export const getStaticProps = getStaticPropsFromFile('content/whbg.md');
 
 export default function WHBG({ fileContent }: { fileContent: string }) {
-  return (
-    <>
-    <Helmet>
-      <title>{ TITLE }</title>
-    </Helmet>
-      <Navbar />
-	  <div>
-		<h1 style={{justifySelf: "center", fontSize: "xx-large", marginBottom: "0.5%"}}>Weird Hamster Ball Game</h1>
-		<div className="article-content">
-			<MarkdownText markdownText={fileContent} />
-		</div>
-	  </div>
-    </>
-  );
+	return (
+		<>
+		<Helmet>
+			<title>{ TITLE }</title>
+		</Helmet>
+		<Navbar />
+		<ProjectContent title="Weird Hamster Ball Game" markdownContent={fileContent} />
+		</>
+	);
 }
