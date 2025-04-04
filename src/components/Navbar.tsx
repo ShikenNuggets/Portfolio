84fe44543de0e1681d7b4483851597bb39231ac8
ShikenNuggets <carter.rennick@gmail.com>
2025-04-03 20:58:26 -0400

    Disable text decoration in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,13 +6,13 @@ const Navbar = () => {
 	return (
 		<>
 			<nav className='w-full fixed-top px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 navbar'>
-				<Link href="/">
+				<Link href="/" style={{ textDecoration: 'none' }}>
 					<h1>Games by Carter</h1>
 				</Link>
 				<ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
-					<li><h1><Link href="/">Home</Link></h1></li>
-					<li><h1><a href="#about">About Me</a></h1></li>
-					<li><h1><a href="#work">My Work</a></h1></li>
+					<li><h1><Link href="/" style={{ textDecoration: 'none' }}>Home</Link></h1></li>
+					<li><h1><a href="#about" style={{ textDecoration: 'none' }}>About Me</a></h1></li>
+					<li><h1><a href="#work" style={{ textDecoration: 'none' }}>My Work</a></h1></li>
 				</ul>
 
 				<div>
