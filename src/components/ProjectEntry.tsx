80deaa2b17591598b2d8c7e89000da25861202e9
ShikenNuggets <carter.rennick@gmail.com>
2025-04-23 18:30:43 -0400

    largeImage option for ProjectEntry

--- src/components/ProjectEntry.tsx
+++ src/components/ProjectEntry.tsx
@@ -9,16 +9,24 @@ interface ProjectProps{
 	tech: string;
 	years: string;
 	shortName: string;
+	largeImage?: boolean;
 }
 
-function ProjectEntry({ title, type, tech, years, shortName }: ProjectProps){
+function ProjectEntry({ title, type, tech, years, shortName, largeImage = false }: ProjectProps){
 	const articleLink = 'projects/' + shortName;
 	const imageName = '/' + shortName + '.png';
+
+	let width = 600;
+	let height = 400;
+	if(largeImage){
+		width = 800;
+		height = 600;
+	}
 
 	return(
 		<div className='project-container'>
 			<Link href={articleLink}>
-				<Image src={imageName} width={600} height={400} alt={title} />
+				<Image src={imageName} width={width} height={height} alt={title} />
 				<div className='project-text'>
 					<h2>{type}</h2>
 					<h2>{tech}</h2>
