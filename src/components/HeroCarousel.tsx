b9c41f24786ce9dd42bc4dc89800cac22f7c225f
ShikenNuggets <carter.rennick@gmail.com>
2025-04-24 20:22:02 -0400

    Exposed priority attribute for images

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -32,7 +32,7 @@ export default function HeroCarousel(){
 		dotListClass="custom-dot-list-style"
 		itemClass="carousel-item-padding-40-px"
 		>
-        <div><ProjectEntry title='Clay Software Corporation' type='Clay Software Corporation' tech='UE5 / C++' years='2024 - Present' shortName='clay' /></div>
+        <div><ProjectEntry title='Clay Software Corporation' type='Clay Software Corporation' tech='UE5 / C++' years='2024 - Present' shortName='clay' priority={true} /></div>
         <div><ProjectEntry title='Disney Magic Kingdoms' type='Gameloft Toronto' tech='C++' years='2019 - 2024' shortName='dmk' /></div>
       </Carousel>
 	  </div>
--- src/components/ProjectEntry.tsx
+++ src/components/ProjectEntry.tsx
@@ -9,16 +9,17 @@ interface ProjectProps{
 	tech: string;
 	years: string;
 	shortName: string;
+	priority?: boolean;
 }
 
-function ProjectEntry({ title, type, tech, years, shortName}: ProjectProps){
+function ProjectEntry({ title, type, tech, years, shortName, priority = false }: ProjectProps){
 	const articleLink = 'projects/' + shortName;
 	const imageName = '/' + shortName + '.png';
 
 	return(
 		<div className='project-container'>
 			<Link href={articleLink}>
-				<Image src={imageName} width={1200} height={800} alt={title} />
+				<Image src={imageName} width={1200} height={800} alt={title} priority={priority} />
 				<div className='project-text'>
 					<h2>{type}</h2>
 					<h2>{tech}</h2>
